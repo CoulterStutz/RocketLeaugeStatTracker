191f7ffcb4463150fe99f704b320a2db440d2a52
coulterminer <coultercash@gmail.com>
2024-01-25 13:17:00 -0700

    [Lables]: Confirm directory exists

--- __main__.py
+++ __main__.py
@@ -1,3 +1,12 @@
 import config
 import termcolor
 import API as a
+import os
+
+if not os.path.exists("\\out"):
+    os.mkdir("out")
+    selfLabels = open("/out/selflabels.txt", "w+")
+    selfLabels = open("/out/opponentlables.txt", "w+")
+else:
+    selfLabels = open("/out/selflabels.txt", "a")
+    selfLabels = open("/out/opponentlables.txt", "a")
