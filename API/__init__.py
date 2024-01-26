6a1fe4136662df665e15f11fd6f42fe9b95f5fd6
coulterminer <coultercash@gmail.com>
2024-01-25 17:05:44 -0700

    [API]: Change RocketLeauge.makeAPIRequest() to RocketLeauge.MakeRankedAPIRequest()

--- API/__init__.py
+++ API/__init__.py
@@ -4,7 +4,7 @@ import requests
 class RocketLeague():
     def __init__(self, player_name, apiSettings, trackingSettings):
         self.url = "https://rocket-league1.p.rapidapi.com/ranks/psych0_naught"
-        self.headers = {
+        self.rankedheaders = {
             "User-Agent": "RapidAPI Playground",
             "Accept-Encoding": "identity",
             "X-RapidAPI-Key": apiSettings["APIKey"],
@@ -15,6 +15,6 @@ class RocketLeague():
         self.api = apiSettings
         self.trackingSettings = trackingSettings
 
-    def makeAPIRequest(self):
-        response = requests.get(self.url, headers=self.headers)
+    def makeRankedAPIRequest(self):
+        response = requests.get(self.url, headers=self.rankedheaders)
         return response.json()
